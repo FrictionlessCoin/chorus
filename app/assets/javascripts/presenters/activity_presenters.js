335b88a419b8e1a692d480d4301b5a302970d163
Michel Milano <mmilano+git@gmail.com>
2015-01-17 12:29:29 -0800

    image file alterations, including - archiving - moving to messaging folder - default user image

--- app/assets/javascripts/presenters/activity_presenters.js
+++ app/assets/javascripts/presenters/activity_presenters.js
@@ -13,7 +13,7 @@
 
         iconSrc: function() {
             if (this.isFailure()) {
-                return "/images/message_error_med.png";
+                return "/images/messaging/message_error_med.png";
             } else if (this.model.isSuccessfulImport()) {
                 return "/images/import_icon.png";
             } else {
