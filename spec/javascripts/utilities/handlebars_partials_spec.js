9296ce79cb2321309e4f205aec42d8d57df828cf
Michel Milano <mmilano+git@gmail.com>
2015-01-28 09:49:40 -0800

    another trial

--- spec/javascripts/utilities/handlebars_partials_spec.js
+++ spec/javascripts/utilities/handlebars_partials_spec.js
@@ -4,27 +4,8 @@ describe("handlebars partials", function () {
             beforeEach(function() {
                 this.context = {};
             });
-
-            it("renders a hidden empty div (1)", function() {
-                var el = Handlebars.VM.invokePartial(Handlebars.partials.errorDiv, "errorDiv", this.context, Handlebars.helpers, Handlebars.partials);
-                expect(el.html()).toContain('<div class="errors hidden">');
-            });
-
-            it("renders a hidden empty div (2)", function() {
-                var el = Handlebars.VM.invokePartial(Handlebars.partials.errorDiv, "errorDiv", this.context, Handlebars.helpers, Handlebars.partials);
-                expect(el.view.$("div.errors.hidden")).toExist();
-            });
-
-            it("renders a hidden empty div (3)", function() {
-                var el = Handlebars.VM.invokePartial(Handlebars.partials.errorDiv, "errorDiv", this.context, Handlebars.helpers, Handlebars.partials);
-                expect(el.$("div")).toHaveClass("hidden");
-            });
-            it("renders a hidden empty div (4)", function() {
-                var el = Handlebars.VM.invokePartial(Handlebars.partials.errorDiv, "errorDiv", this.context, Handlebars.helpers, Handlebars.partials);
-                expect(el.find("div").html()).toContain('class="errors hidden"');
-            });
             
-             it("renders a hidden empty div (5)", function() {
+             it("renders a hidden empty div ", function() {
                 var el = Handlebars.VM.invokePartial(Handlebars.partials.errorDiv, "errorDiv", this.context, Handlebars.helpers, Handlebars.partials);
                 expect(el).toContain('<div class="errors hidden">');
             });
